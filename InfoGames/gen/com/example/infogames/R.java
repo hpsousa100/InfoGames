d7d6613f08700da2a403904c374ce1f093a846b3
hpsousa100 <hpsousa100@gmail.com>
2016-03-31 14:54:51 -0300

    reupado

--- InfoGames/gen/com/example/infogames/R.java
+++ InfoGames/gen/com/example/infogames/R.java
@@ -22,7 +22,9 @@ public final class R {
     }
     public static final class drawable {
         public static final int dark=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
+        public static final int darkmaiort=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
+        public static final int iconeappteste2=0x7f020003;
     }
     public static final class id {
         public static final int action_settings=0x7f080002;
